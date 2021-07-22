4b506c35d63fb630cdda01d48c4b17e70f3b118e
sngina <sngina707@gmail.com>
2021-07-22 10:31:50 +0300

    add class models

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    def save_course(self):
+        self.save()
+
+    @classmethod
+    def delete_course(cls , id):
+        cls.objects.filter(id=id).delete()
+
+       
