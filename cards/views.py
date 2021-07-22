082759f4f4c71469e4b74a6c59681471c449f6f6
sngina <sngina707@gmail.com>
2021-07-22 10:38:55 +0300

    add views

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from .models import  Course
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url= '/accounts/login/')
+def flash_post(request):
+    flash = Course.objects.all()
+    if request.method == 'POST':
+        return redirect('homepage')
--- flashcards/settings.py
+++ flashcards/settings.py
@@ -15,7 +15,7 @@ import os
 import django_heroku
 import dj_database_url
 from decouple import config, Csv
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
